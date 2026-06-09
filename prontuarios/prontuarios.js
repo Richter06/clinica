fcf921ac95be1ebc47b62a7033c2a2198fdf98f1
Richter06 <richardrodriguis09@gmail.com>
2026-06-09 19:50:11 -0300

    adiciona funcionalidade de toggle para o menu lateral responsivo na página de prontuários

--- prontuarios/prontuarios.js
+++ prontuarios/prontuarios.js
@@ -159,3 +159,16 @@ function excluirProntuario(index) {
 carregarPacientes();
 carregarMedicos();
 render();
+
+const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
+
+menuToggle.addEventListener("click", () => {
+
+    sidebar.classList.toggle("open");
+    menuToggle.classList.toggle("active");
+
+    menuToggle.textContent =
+        sidebar.classList.contains("open") ? "✕" : "☰";
+
+});
