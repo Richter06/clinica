fef831dd72239bdcc67086e9941f3bf0e4497350
Richter06 <richardrodriguis09@gmail.com>
2026-06-09 21:05:47 -0300

    adiciona funcionalidade de alternância de tema com ícone correspondente na consultas

--- consultas/consultas.js
+++ consultas/consultas.js
@@ -176,6 +176,8 @@ pesquisa.addEventListener("input",(e)=>{
 
 renderizar();
 
+
+// MENU HAMBURGUER
 const menuToggle = document.getElementById("menuToggle");
 const sidebar = document.getElementById("sidebar");
 
@@ -187,4 +189,20 @@ menuToggle.addEventListener("click", () => {
     menuToggle.textContent =
         sidebar.classList.contains("open") ? "✕" : "☰";
 
+});
+
+//THEME TOGGLE
+
+const themeToggle = document.getElementById("themeToggle");
+const themeIcon = document.getElementById("themeIcon");
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        themeIcon.src = "../icons/dark.png";
+    }else{
+        themeIcon.src = "../icons/light.png";
+    }
 });
