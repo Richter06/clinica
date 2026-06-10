2699d58dde60ebcc047472f819da3857903821a7
Richter06 <richardrodriguis09@gmail.com>
2026-06-09 21:18:33 -0300

    adiciona funcionalidade de alternância de tema com botão e ícone correspondente em relatórios

--- relatorios/relatorios.js
+++ relatorios/relatorios.js
@@ -108,6 +108,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+// MENU HAMBURGUER
+
 const menuToggle = document.getElementById("menuToggle");
 const sidebar = document.getElementById("sidebar");
 
@@ -119,4 +121,20 @@ menuToggle.addEventListener("click", () => {
     menuToggle.textContent =
         sidebar.classList.contains("open") ? "✕" : "☰";
 
+});
+
+// THEME TOGGLE
+
+const themeToggle = document.getElementById("themeToggle");
+const themeIcon = document.getElementById("themeIcon");
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(document.body.classList.contains("dark-mode")){
+        themeIcon.src = "../icons/dark.png";
+    }else{
+        themeIcon.src = "../icons/light.png";
+    }
 });
